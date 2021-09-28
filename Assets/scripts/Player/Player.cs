956506c43570ab28d41fd23d925b148705d602b7
tanku_for_gitBash <57029371+tanku3080@users.noreply.github.com>
2021-09-29 02:35:55 +0900

    ゲームのマップを追加。移動でカメラも追従するようにしたが、不具合が発生

--- Assets/scripts/Player/Player.cs
+++ Assets/scripts/Player/Player.cs
@@ -6,7 +6,10 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     new Rigidbody rigidbody;
-    [SerializeField] public float playerSpeed = 5f;
+    [SerializeField] float playerSpeed = 5f;
+    [SerializeField] float rotationSpeed = 10f;
+    /// <summary>角度制限の値</summary>
+    [SerializeField] float limitRotY = 70;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +21,14 @@ public class Player : MonoBehaviour
         //一人称ゲームなので等速移動を行うようにしている
         var v = Input.GetAxis("Vertical") * playerSpeed;
         var h = Input.GetAxis("Horizontal") * playerSpeed;
+        var rotX = Input.GetAxis("Mouse X") * rotationSpeed;
+        var rotY = Input.GetAxis("Mouse Y") * rotationSpeed;
 
         rigidbody.velocity = new Vector3(h, 0, v);
+        transform.Rotate(0,rotX,0);
+        if (rotY >= -limitRotY && rotY <= limitRotY)
+        {
+            transform.localEulerAngles = new Vector3(rotY,0,0);
+        }
     }
 }
